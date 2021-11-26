a0d4d3a7135e33c7003a0f23192d68606739542f
A100610 <A100610@alunos.uminho.pt>
2021-11-26 11:41:41 +0000

    funcao mapa tarefa3

--- src/Tarefa3_2021li1g092.hs
+++ src/Tarefa3_2021li1g092.hs
@@ -9,6 +9,26 @@ Módulo para a realização da Tarefa 3 do projeto de LI1 em 2021/22.
 module Tarefa3_2021li1g092 where
 
 import LI12122
+import Funcoesuteis
 
 instance Show Jogo where
   show = undefined
+
+-- :: Jogador -> Mapa -> String
+
+
+
+
+mapa :: Mapa -> String
+mapa [[]] = []
+mapa [(h:t)] = mapa' (h:t) 
+mapa ((h:t):tt) = mapa' (h:t) ++ "\n" ++ mapa tt
+
+mapa' :: [Peca] -> String
+mapa' [] = []
+mapa' (h:t)  
+  |h == Caixa = "C" ++ mapa' t 
+  |h == Porta = "P" ++ mapa' t 
+  |h == Bloco = "X" ++ mapa' t 
+  |otherwise = " " ++ mapa' t 
+
