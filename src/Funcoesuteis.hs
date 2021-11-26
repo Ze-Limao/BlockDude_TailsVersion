bb5ddfa90da31e0d7c819b8db919acfedae07c01
A100610 <A100610@alunos.uminho.pt>
2021-11-26 20:45:21 +0000

    Tarefa3 finalizada efuncous aux atualizadas

--- src/Funcoesuteis.hs
+++ src/Funcoesuteis.hs
@@ -22,7 +22,7 @@ bloconext (a, (b, c))  (d, (e, f))
 lowerblock :: [(Peca, Coordenadas)] -> (Peca, Coordenadas)
 lowerblock [(p,(x,y))] = (p,(x,y))
 lowerblock ((a, (x1, y1)) : (b, (x2, y2)) : t)
-  |x1 > x2 = lowerblock ((a, (x1, y1)) : t)
+  |y1 >= y2 = lowerblock ((a, (x1, y1)) : t)
   |otherwise = lowerblock ((b, (x2, y2)) : t)
 
 coluna :: Int -> [(Peca, Coordenadas)] -> [(Peca, Coordenadas)]
@@ -44,6 +44,10 @@ isCaixa ((p,(x,y)):t)
   |p == Caixa = (p,(x,y)) : isCaixa t 
   |otherwise = isCaixa t 
 
+
+mylength :: [a] -> Int
+mylength [] = 0
+mylength (x:xs) = 1 + mylength xs
 
 --vazios :
 nVazios :: [(Peca, Coordenadas)] -> Int
@@ -79,3 +83,22 @@ altura [(a, (b, c))] = c
 altura ((a, (b, c)) : (d, (e, f)) : xs)
   | c > f = altura ((a, (b, c)) : xs)
   | otherwise = altura ((d, (e, f)) : xs)
+
+-- Tarefa3:
+--deconstroi mapa da tarefa 2
+decontroiMapa :: Mapa -> [(Peca, Coordenadas)]
+decontroiMapa mapa = decontroiMapa' (0,0) mapa  
+
+decontroiMapa' :: Coordenadas -> Mapa -> [(Peca, Coordenadas)]
+decontroiMapa' (x,y) [[]]  = []
+decontroiMapa' (x,y) ([]:xs)  = decontroiMapa' (0,y+1) xs 
+decontroiMapa' (x,y) ((h:t) : xs)   
+    |h == Vazio = decontroiMapa' (x+1,y) (t:xs) 
+    |otherwise = (h,(x,y)) : decontroiMapa' (x+1,y) (t:xs)
+
+insertAt :: [a] -> a -> Int -> [a]
+insertAt [] elem pos = [elem]
+insertAt (x:xs) elem pos
+    | pos == 0 = elem : x : xs
+    | pos > 0 = x : insertAt xs elem (pos - 1) 
+    | otherwise = x : insertAt xs elem ((pos) + length (x:xs) )
--- src/Tarefa3_2021li1g092.hs
+++ src/Tarefa3_2021li1g092.hs
@@ -9,26 +9,66 @@ Módulo para a realização da Tarefa 3 do projeto de LI1 em 2021/22.
 module Tarefa3_2021li1g092 where
 
 import LI12122
-import Funcoesuteis
-
+import Funcoesuteis 
+{-}
 instance Show Jogo where
-  show = undefined
+  show (Jogo mapa (Jogador (x,y) dir caixa))
+    |show (Jogo [[]] (Jogador (x,y) dir caixa)) = "" 
+    |otherwise = show mapa (Jogador (x,y) dir caixa)
+                  |espacojogador mapa (Jogador (x,y) dir caixa) == False = ""
+                  |espacojogador mapa (Jogador (x,y) dir caixa) == True = jogo' (mapear mapa) (Jogador (x,y) dir caixa)
+-}
 
--- :: Jogador -> Mapa -> String
+jogo :: Mapa -> Jogador -> String
+jogo [[]] (Jogador (x,y) dir caixa) = "" 
+jogo mapa (Jogador (x,y) dir caixa)
+  |espacojogador mapa (Jogador (x,y) dir caixa) == False = ""
+  |espacojogador mapa (Jogador (x,y) dir caixa) == True = jogo' (mapear mapa) (Jogador (x,y) dir caixa)
+
+jogo' :: String -> Jogador -> String 
+jogo' str (Jogador (x,y) dir caixa) = insertAt str (direcaodude (Jogador (x,y) dir caixa)) (posicaodude str (Jogador (x,y) dir caixa))
+
+posicaodude :: String -> Jogador -> Int
+posicaodude str (Jogador (x,y) dir caixa) = ((lengthX(lengthX' str)+1)  * y) + x + y
+
+lengthX :: String -> Int
+lengthX "" = 0
+lengthX (h:t) = 1 + lengthX t
+
+lengthX' :: String -> String
+lengthX' "" = ""
+lengthX' (h:t) 
+  |h == '\n' = ""
+  |otherwise = "a" ++ lengthX' t
+
+direcaodude :: Jogador -> Char
+direcaodude (Jogador (x,y) dir caixa) 
+  |dir == Oeste = '<'
+  |otherwise = '>'
+
+espacojogador :: Mapa -> Jogador -> Bool
+espacojogador mapa (Jogador (x,y) dir caixa) 
+  |caixa == True = espacojogador' (decontroiMapa mapa) (Jogador (x,y-1) dir caixa) == 1 && espacojogador' (decontroiMapa mapa) (Jogador (x,y) dir caixa) == 1
+  |otherwise = espacojogador' (decontroiMapa mapa) (Jogador (x,y) dir caixa) == 1
+
+--tem de dar 1 para q o dude tenha espaco 
+espacojogador' :: [(Peca,Coordenadas)] -> Jogador -> Int
+espacojogador' [] j = 1
+espacojogador' ((p,(a,b)):t) (Jogador (x,y) dir caixa)
+  |a==x && b==y = 1 + espacojogador' t (Jogador (x,y) dir caixa)
+  |otherwise = espacojogador' t (Jogador (x,y) dir caixa)
 
 
+mapear :: Mapa-> String
+mapear [[]] = ""
+mapear [(h:t)] = mapear' (h:t) 
+mapear ((h:t):tt) = mapear' (h:t) ++ "\n" ++ mapear tt
 
-
-mapa :: Mapa -> String
-mapa [[]] = []
-mapa [(h:t)] = mapa' (h:t) 
-mapa ((h:t):tt) = mapa' (h:t) ++ "\n" ++ mapa tt
-
-mapa' :: [Peca] -> String
-mapa' [] = []
-mapa' (h:t)  
-  |h == Caixa = "C" ++ mapa' t 
-  |h == Porta = "P" ++ mapa' t 
-  |h == Bloco = "X" ++ mapa' t 
-  |otherwise = " " ++ mapa' t 
+mapear' :: [Peca] -> String
+mapear' [] = ""
+mapear' (h:t)  
+  |h == Caixa = "C" ++ mapear' t 
+  |h == Porta = "P" ++ mapear' t 
+  |h == Bloco = "X" ++ mapear' t 
+  |otherwise = " " ++ mapear' t 
 
