dd8ceb8e966cb4c12cabcc2e14e47df7e9537bd4
ZE CORREIA <a100610@alunos.uminho.pt>
2021-11-27 15:39:31 +0000

    Replace Tarefa2_2021li1g092.hs

--- src/Tarefa2_2021li1g092.hs
+++ src/Tarefa2_2021li1g092.hs
@@ -11,20 +11,8 @@ module Tarefa2_2021li1g092 where
 import LI12122
 import Funcoesuteis
 
---constroiMapa :: [(Peca, Coordenadas)] -> Mapa
-
-
---ver uma linha uma a uma e fazer a line , depois disso ,juntar todas as lines (cosntroimapa)
-
-line :: [(Peca, Coordenadas)] -> [(Peca)]
-line ((p,(x,y)):t) = line' (0,0) ((p,(x,y)):t)  
-
-line' :: Coordenadas -> [(Peca, Coordenadas)] -> [(Peca)]
-line' (a,b) ((p,(x,y)):t) 
-    |a == x = p : line' ((a+1),b) t
-    |a /= x = Vazio : line' ((a+1),b) ((p,(x,y)):t)
-    |otherwise = []
-
+constroiMapa :: [(Peca, Coordenadas)] -> Mapa
+constroiMapa pecas = undefined
 
 
 desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
@@ -36,3 +24,6 @@ desconstroiMapa' (x,y) ([]:xs)  = desconstroiMapa' (0,y+1) xs
 desconstroiMapa' (x,y) ((h:t) : xs)   
     |h == Vazio = desconstroiMapa' (x+1,y) (t:xs) 
     |otherwise = (h,(x,y)) : desconstroiMapa'  (x+1,y) (t:xs)
+
+
+
