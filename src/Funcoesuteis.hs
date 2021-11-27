4cce2b7db83d61d24b49254f7d39f06a382e887b
A100610 <A100610@alunos.uminho.pt>
2021-11-27 13:35:02 +0000

    funcoes auxiliares

--- src/Funcoesuteis.hs
+++ src/Funcoesuteis.hs
@@ -53,28 +53,6 @@ mylength :: [a] -> Int
 mylength [] = 0
 mylength (x:xs) = 1 + mylength xs
 
---vazios :
-nVazios :: [(Peca, Coordenadas)] -> Int
-nVazios [] = 0
-nVazios l = area l - nPecas l
-
-nPecas :: [(Peca,Coordenadas)] -> Int
-nPecas [] = 0
-nPecas ((p,(x,y)):t) = 1 + nPecas t
-
-area :: [(Peca,Coordenadas)] -> Int
-area [] = 0
-area l = area' (ultpeca l)
-
-area' :: Coordenadas -> Int
-area' (x,y) = x*y
-
-ultpeca :: [(Peca,Coordenadas)] -> Coordenadas
-ultpeca [] = (0,0)
-ultpeca [(p,(x,y))] = (x+1,y+1)
-ultpeca ((p,(x,y)):t) 
-    |x == comprimento [(p,(x,y))] && y == altura [(p,(x,y))] = (x,y)
-    |otherwise = ultpeca t
 
 comprimento :: [(Peca, Coordenadas)] -> Int
 comprimento [(a, (b, c))] = b
