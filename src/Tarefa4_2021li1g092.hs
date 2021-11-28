6d985d84aa7b67e2d8ef113a5a3cfe33e8b0ed8e
A100610 <A100610@alunos.uminho.pt>
2021-11-28 12:55:45 +0000

    ja funciona bem

--- src/Tarefa4_2021li1g092.hs
+++ src/Tarefa4_2021li1g092.hs
@@ -17,24 +17,23 @@ correrMovimentos jogo [] = jogo
 correrMovimentos jogo (x:xs) = correrMovimentos (moveJogador jogo x) xs
 
 moveJogador :: Jogo -> Movimento -> Jogo 
-moveJogador (mapa (Jogador (x,y) Oeste caixa)) move = undefined
-
-{-}
-onemove :: Jogo -> Movimento -> Jogo 
-onemove mapa (Jogador (x,y) dir caixa) move 
-    |move == AndarDireita = (mapa(Jogador (x+1,y) Este caixa))
-    |move == AndarEsquerda = (mapa(Jogador (x-1,y) Oeste caixa))
-    |move == Trepar = (mapa(Jogador (x-1,y+1) dir caixa)) 
- --sss falta a interage caixa
--}
+moveJogador (Jogo mapa (Jogador (x,y) dir caixa)) move 
+    |move == AndarDireita = cair (Jogo mapa(Jogador (x+1,y) Este caixa)) (AndarDireita)
+    |move == AndarEsquerda = cair (Jogo mapa(Jogador (x-1,y) Oeste caixa)) (AndarEsquerda)
+    |move == Trepar = (Jogo mapa(Jogador (x-1,y-1) dir caixa)) 
+    |otherwise = (Jogo mapa (Jogador (x,y) dir caixa))  --falta apenas tratar da interage caixa
 
 cair :: Jogo -> Movimento -> Jogo
-cair mapa (Jogador (x,y) dir caixa) move
-    |move == AndarEsquerda = cair' (decontroiMapa mapa) (Jogador (x-1,y) dir caixa)
-    |otherwise = cair' (decontroiMapa mapa) (Jogador (x+1,y)) --ss
+cair (Jogo mapa (Jogador (x,y) dir caixa)) move = (Jogo mapa (cair' (decontroiMapa mapa) (Jogador (x,y) dir caixa)))
 
+cair' :: [(Peca,Coordenadas)] -> Jogador -> Jogador
+cair' l (Jogador (x,y) dir caixa)
+    |(Jogador (x,y) dir caixa) /= cair'' l (Jogador (x,y) dir caixa) = cair' l (cair'' l (Jogador (x,y+1) dir caixa))
+    |otherwise = cair'' l (Jogador (x,y) dir caixa)
 
-cair' :: [(Peca,Coordenadas)] -> Jogador -> Jogo
-cair' ((h,(a,b)):t) (Jogador (x,y) dir caixa) 
-    |a==x && b == (y+1) = (h,(a,b):t) (Jogador (x,y) dir caixa)
-    |otherwise = cair' t (Jogador (x,y+1) dir caixa)
+cair'' :: [(Peca,Coordenadas)] -> Jogador -> Jogador
+cair'' [] (Jogador (x,y) dir caixa) = (Jogador (x,y+1) dir caixa)
+cair'' ((h,(a,b)):t) (Jogador (x,y) dir caixa) 
+    |a==x && b == (y+1) = (Jogador (x,y) dir caixa)
+    |otherwise = cair'' t (Jogador (x,y) dir caixa)
+
