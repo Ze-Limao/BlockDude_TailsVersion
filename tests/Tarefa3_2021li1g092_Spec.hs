941416e04e4ece739a6939a8ba59b066ab581714
A100610 <A100610@alunos.uminho.pt>
2021-11-28 17:33:37 +0000

    terminado

--- tests/Tarefa3_2021li1g092_Spec.hs
+++ tests/Tarefa3_2021li1g092_Spec.hs
@@ -8,4 +8,8 @@ testsT3 =
   test
     [ "Tarefa 3 - Teste Imprime Jogo m1e1" ~: "      <\n      X\n      X\nP   C X\nXXXXXXX" ~=?  show m1e1
     , "Tarefa 3 - Teste Imprime Jogo m1e2" ~: "       \n      X\n      X\nP < C X\nXXXXXXX" ~=?  show m1e2
+    , "Tarefa 3 - Teste Imprime Jogo Jogador Este" ~: "       \n      X\n      X\nP > C X\nXXXXXXX" ~=?  show m1e3
+    , "Tarefa 3 - Teste Imprime Jogo onde o Jogador sem caixa nao tem espaco para nascer" ~: "" ~=? show m1e4
+    , "Tarefa 3 - Teste Imprime Jogo em que o jogador apenas nao tem espaco se estiver com a caixa " ~: "" ~=? show m1e5
     ]
+    
--- tests/Tarefa4_2021li1g092_Spec.hs
+++ tests/Tarefa4_2021li1g092_Spec.hs
@@ -8,26 +8,10 @@ import Fixtures
 
 testsT4 =
   test
-    [ "Tarefa 4 - Teste Move m1e1 Oeste" ~: Jogo m1r (Jogador (5, 3) Oeste False) ~=?  moveJogador m1e1 AndarEsquerda
-    , "Tarefa 4 - Teste Move m1e1 Este" ~: Jogo m1r (Jogador (6, 0) Este False) ~=?  moveJogador m1e1 AndarDireita
-    , "Tarefa 4 - Teste Move m1e1 Trepar" ~: m1e1 ~=? moveJogador m1e1 Trepar
-    , "Tarefa 4 - Teste Move m1e1 InterageCaixa" ~: m1e1 ~=?  moveJogador m1e1 InterageCaixa
-    , "Tarefa 4 - Teste movimentos m1e1" ~: m1e2 ~=?  correrMovimentos m1e1 [AndarEsquerda, Trepar, AndarEsquerda, AndarEsquerda]
-    , "Tarefa 4 - Teste movimentos m1e2 Caixa1" ~: Jogo
-        [ [Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio]
-        , [Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco]
-        , [Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco]
-        , [Porta, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco]
-        , [Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]
-        ]
-        (Jogador (3, 3) Este True) ~=?  correrMovimentos m1e2 [AndarDireita, InterageCaixa]
-    , "Tarefa 4 - Teste movimentos m1e2 Caixa2" ~:
-      Jogo
-        [ [Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio]
-        , [Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco]
-        , [Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco]
-        , [Porta, Caixa, Vazio, Vazio, Vazio, Vazio, Bloco]
-        , [Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]
-        ]
-        (Jogador (2, 3) Oeste False) ~=?  correrMovimentos m1e2 [AndarDireita, InterageCaixa, AndarEsquerda, InterageCaixa]
+    [ "Tarefa 4 - Teste move m1e1 Oeste e queda" ~: Jogo m1r (Jogador (5,3) Oeste False) ~=?  moveJogador m1e1 AndarEsquerda
+    , "Tarefa 4 - Teste move m1e1 Este" ~: Jogo m1r (Jogador (3,3) Este False) ~=?  moveJogador m1e2 AndarDireita
+    , "Tarefa 4 - Teste move m1e1 nao ter o que Trepar" ~: m1e1 ~=? moveJogador m1e1 Trepar
+    , "Tarefa 4 - Teste moves esquerda" ~: m1e2 ~=?  correrMovimentos m1e1 [AndarEsquerda, Trepar, AndarEsquerda, AndarEsquerda]    
+    , "Tarefa 4 - Teste moves direita " ~: Jogo m1r (Jogador (5,3) Este False) ~=? correrMovimentos m1e2 [AndarDireita, Trepar, AndarDireita]
+    , "Tarefa 4 - Teste moves ir contra uma parede ou caixa" ~: Jogo m1r (Jogador (5,3) Oeste False) ~=? correrMovimentos m1e1 [AndarEsquerda,AndarDireita,AndarDireita,AndarEsquerda]
     ]
