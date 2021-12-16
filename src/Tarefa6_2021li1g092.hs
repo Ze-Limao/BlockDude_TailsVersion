2562bbfa14e3772629979f1604c765e92410e28d
A100610 <A100610@alunos.uminho.pt>
2021-12-16 11:40:47 +0000

    continuação da tarefa 5

---
+++ src/Tarefa6_2021li1g092.hs
@@ -0,0 +1,12 @@
+{- |
+Module      : Tarefa6_2021li1gXXX
+Description : Resolução de um puzzle
+
+Módulo para a realização da Tarefa 6 do projeto de LI1 em 2021/22.
+-}
+module Tarefa6_2021li1gXXX where
+
+import LI12122
+
+resolveJogo :: Int -> Jogo -> Maybe [Movimento]
+resolveJogo i jogo = undefined
