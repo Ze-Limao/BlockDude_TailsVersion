80dbc872d78e212eeaf63e26aa0e275d075ff534
A100610 <A100610@alunos.uminho.pt>
2021-12-20 15:09:46 +0000

    jogos e mapas

---
+++ src/Mapas.hs
@@ -0,0 +1,9 @@
+module Mapas where 
+import LI12122
+
+
+jogo01 = (Jogo[[Bloco,Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Bloco,Porta, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Bloco,Bloco, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Bloco,Bloco, Vazio, Vazio, Vazio, Vazio, Vazio, Bloco],[Bloco,Bloco, Vazio, Caixa, Vazio, Caixa, Vazio, Bloco],[Bloco,Bloco, Bloco, Bloco, Bloco, Bloco, Bloco, Bloco]] (Jogador (6,4) Oeste True))
+--JOGO 1 nao vai ser este
+
+jogo1 = (Jogo [[Vazio,Bloco],[Vazio,Bloco,Vazio,Vazio,Vazio,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco],[Bloco,Vazio,Bloco,Vazio,Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Bloco],[Bloco,Vazio,Vazio,Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Bloco],[Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Caixa,Bloco],[Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Caixa,Caixa,Bloco],[Bloco,Vazio,Bloco,Bloco,Bloco,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Vazio,Bloco,Caixa,Vazio,Bloco,Bloco],[Bloco,Vazio,Bloco,Vazio,Bloco,Vazio,Vazio,Vazio,Vazio,Bloco,Vazio,Vazio,Bloco,Bloco,Bloco,Bloco,Bloco],[Bloco,Vazio,Bloco,Vazio,Bloco,Caixa,Caixa,Vazio,Bloco,Bloco,Vazio,Vazio,Bloco],[Bloco,Porta,Bloco,Vazio,Bloco,Bloco,Bloco,Bloco,Bloco,Bloco,Vazio,Bloco,Bloco],[Bloco,Bloco,Bloco,Vazio,Bloco,Bloco,Vazio,Vazio,Vazio,Bloco,Bloco,Bloco]](Jogador (5,7) Este False))
+
